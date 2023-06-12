abb7dfefa0392f46819fbb1cfe370618973a848f
d.karyakin <3754479555552@gmail.com>
2023-06-12 17:31:11 +0300

    init

---
+++ Consumer.ERROR/Consumer.ERROR/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+using var connection = factory.CreateConnection();
+using var chanel = connection.CreateModel();
+
+chanel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+var queueName = chanel.QueueDeclare().QueueName;
+chanel.QueueBind(
+    queue: queueName,
+    exchange: "direct_logs",
+    routingKey: "error"
+    );
+    
+var consumer = new EventingBasicConsumer(chanel);
+    
+consumer.Received += (sender, e) =>
+{
+    var body = e.Body;
+    var message = Encoding.UTF8.GetString(body.ToArray());
+    Console.WriteLine($"Received message {message}");
+};
+//Связываем конзьюмера с очередью
+chanel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer
+);
+Console.ReadKey();
---
+++ Publiser/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+Task.Run(CreateTask(2000, "error"));
+Task.Run(CreateTask(3000, "information"));
+Task.Run(CreateTask(4000, "warning"));
+
+Console.ReadKey();
+
+
+static Func<Task> CreateTask(int timeOutToSleep, string routingKey)
+{
+    return () =>
+    {
+        var count = 0;
+        do
+        {
+            int timeToSleep = new Random().Next(1000, timeOutToSleep);
+            Thread.Sleep(timeToSleep);
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+            using var chanel = connection.CreateModel();
+
+            chanel.ExchangeDeclare(
+                exchange: "direct_logs",
+                type: ExchangeType.Direct
+            );
+
+            string message = $"message type: {routingKey}";
+            var body = Encoding.UTF8.GetBytes(message + "№" + count);
+
+            chanel.BasicPublish(
+                exchange: "direct_logs",
+                routingKey: routingKey,
+                basicProperties: null,
+                body: body
+            );
+            
+            Console.WriteLine($"message type: {routingKey}: {count}");
+            count++;
+        } while (true);
+    };
+}
